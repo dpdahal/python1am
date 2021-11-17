ca2e70ca7c4538032440b24c379c5764086399ae
dp48dahal@gmail.com <dp48dahal@gmail.com>
2021-11-17 13:12:05 +0545

    hello github

---
+++ introduction.py
@@ -0,0 +1,30 @@
+# print("Hello python class")
+# print('hello', 'python', 'class')
+# mother's
+
+# print('mother\'s')
+
+# print(1, 'a')
+
+# print("mother's")
+
+# first_name = 'hari'
+# last_name = 'abc'
+# age = 10
+#
+# print("my name is : {} {} age: {}".format(first_name, last_name, age))
+# print(f"my name is : {first_name} {last_name} age: {age}")
+# print(first_name, last_name, age)
+
+
+# x = 10
+#
+# print(type(x))
+# x, y, z = 10, 20, 30
+
+# x = int(input("Enter x number: "))
+# y = int(input("Enter y number: "))
+# # x = int(x)
+# y = int(y)
+# print(x + y)
+
